b1cf92529ab674a0efc2d9b05724e00019deba40
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2022-04-05 22:59:01 +0300

    Инициализация

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.sass'
+import App from './App'
+
+const app = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+app.render(<App />)
