c4cc9456a3fc9d9741ba891e1422bdd007d7d297
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2022-04-07 14:42:22 +0300

    Добавлено отображение ников игроков

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from './Game.module.sass'
 import GamePainter from './GamePainter'
+import { StateContext } from '../../context/state.context'
 
 interface GameProps {}
 
 const Game: React.FC<GameProps> = () => {
+  const { game } = React.useContext(StateContext)
   const containerRef = React.useRef<HTMLDivElement>(null)
   const [containerWidth, setContainerWidth] = React.useState(0)
 
@@ -16,6 +18,10 @@ const Game: React.FC<GameProps> = () => {
 
   return (
     <div className={styles.container} ref={containerRef}>
+      <div className={styles.containerNicknames}>
+        <p className={styles.containerNickname}>Ты - {game?.myNickname}</p>
+        <p className={styles.containerNickname}>Соперник - {game?.rivalNickname}</p>
+      </div>
       <div className={styles.containerGameContainer}>
         <GamePainter containerWidth={containerWidth} />
       </div>
